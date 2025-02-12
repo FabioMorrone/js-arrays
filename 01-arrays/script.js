51de9c1259a94246d34500b5f0cd29f3efd2d9a1
FabioMorrone <morronefabio04@gmail.com>
2025-02-12 16:17:10 +0100

    I did the 7th exercise

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -16,22 +16,38 @@ console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
-teachers.slice('patrick')
+teachers.splice(4, 1, 'patrick')
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+
+const lastTeacher = teachers[6];
+console.log(lastTeacher);
+teachers.pop()
+console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+
+const firstTeacher = teachers[0];
+console.log(firstTeacher);
+teachers.shift(0)
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
+teachers.push('Vanessa')
+console.log(teachers);
+
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+
+teachers.unshift("Sarah")
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf("Lewis");
+console.log(lewisIndex);
+
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
